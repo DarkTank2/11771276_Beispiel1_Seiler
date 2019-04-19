830096ad5426c940c92c40ecb6d58a96b26cd66c
DarkTank2 <x.s@gmx.at>
2019-04-19 17:34:10 +0200

    Added OrderState Enums, failed to add methods valueOf(String) nd values() since implicit declaration

---
+++ Beispiel1/src/rbvs/OrderState.java
@@ -0,0 +1,13 @@
+/**
+ * Filename: OrderState.java
+ * Description: States that can be used by, for example, an Order.
+ * @author Alexander Seiler, 11771276
+ * @since 19.04.2019
+ */
+package rbvs;
+
+public enum OrderState {
+	CANCELLED,
+	OPEN,
+	PAID;
+}
