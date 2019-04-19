5a95e8f1d92123099ddb18d980730114d9761461
DarkTank2 <x.s@gmx.at>
2019-04-19 17:33:26 +0200

    Added DuplicateProductException

---
+++ Beispiel1/src/rbvs/DuplicateProductException.java
@@ -0,0 +1,30 @@
+/**
+ * Filename: DuplicateProductException.java
+ * Description: 
+ * @author Alexander Seiler, 11771276
+ * @since 19.04.2019
+ */
+package rbvs;
+
+import rbvs.product.IProduct;
+
+public class DuplicateProductException extends Exception {
+
+	private IProduct product;
+	@SuppressWarnings("unused")
+	private static long	serialVersionUID = 101101;
+	
+	/**
+	 * Constructor for class DuplicateProductException.java
+	 * @author Alexander Seiler, 11771276
+	 * @param product
+	 */
+	public DuplicateProductException(IProduct product) {
+		this.product = product;
+	}
+	
+	
+	public String getMessage() {
+		return "Product " + this.product.getName() + " already exists!";
+	}
+}
